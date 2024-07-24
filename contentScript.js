4cf537df5a7519c90211287da14c0dd4bc3753d1
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-07-24 18:11:40 -0400

    refactored: popup in dir

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,8 @@
 function disableNoti() {
+  if (window.location.href.includes('github.com/')) {
+    console.log('At github');
+  }
+
   const indicator = document.getElementsByClassName(
     'AppHeader-button--hasIndicator'
   );
@@ -7,16 +11,23 @@ function disableNoti() {
     indicator[0].remove();
   }
 
-  //   if (document.getElementsByTagName("main")) {
-  //     if (document.getElementsByTagName("main")[0]) {
-  //       document.getElementsByTagName("main")[0].remove();
-  //     }
-  //   }
 }
 
 chrome.storage.local.get(['disableNotiIndicator'], (res) => {
+  console.log('res from contentScript', res);
   if (res.disableNotiIndicator === undefined) {
     chrome.storage.local.set({ disableNotiIndicator: true });
     disableNoti();
   }
 });
+
+setInterval(() => {
+  console.log('Every 500');
+  chrome.storage.local.get(['disableNotiIndicator'], (res) => {
+    if (res.disableNotiIndicator) {
+      disableNoti();
+    }
+  });
+  // 0 - 1 ms => nu indicator at all
+
+}, 500);
--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,15 @@
 const checkBoxNoti = document.getElementById('gh-checkbox-noti');
+console.log("Jello from popup");
 
 chrome.storage.local.get(['disableNotiIndicator'], (res) => {
+  console.log("from popup: ",{ res });
   if (res) {
     checkBoxNoti.checked = res.disableNotiIndicator;
   }
 });
 
 checkBoxNoti.addEventListener('change', () => {
+    console.warn("Check box noti changed");
   if (checkBoxNoti.checked) {
     chrome.storage.local.set({ disableNotiIndicator: true });
   } else {
